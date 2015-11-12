e50daaaa7b5b2cde773c0f1fa143354619d30b82
Oleg Hudak <olhudak@gmail.com>
2015-11-12 23:35:07 +0200

    about_array_assigment, about_arrays

--- ent_w.rb
+++ ent_w.rb
@@ -1,21 +1,8 @@
+puts 'Enter your word'
 color = []
-word = 'sss'
-
-while word != ''
-  puts 'Enter your word'
-  word = gets.chomp
-  color.push(word)
-end
-
-swap = true
-size = color.length - 1
-while swap
-    swap = false
-    for i in 0...size
-        swap |= color[i] > color[i + 1] 
-        color[i], color[i + 1] = color[i + 1], color[i] if color[i] > color [i + 1]
-    end
-    size -= 1
-end
-puts color.join(', ')
-
+word = gets.chomp
+ if word != ''
+   color.push(word)
+ else
+   puts color.sort
+ end
